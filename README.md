eda27fb4bae31dcb23e0a86fcec565c34aa17393
cybeR@ <122730833+cybeR-at@users.noreply.github.com>
2026-08-17 19:55:19 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# dash-hover-hold
-GNOME extension that prevents the Dash and Overview from closing automatically when clicking on an application. Useful for opening up multiple applications at once while preserving the native Gnome Dash.
+# 🖱️ Dash Hover Hold 🖱️ 
+
+*To open everything up!*
+
+A GNOME (50) extension that improves general initial multitasking. Natively, GNOME closes the Dash and Overview immediately after you click an app icon on it. With Dash Hover Hold, you can click and launch as many apps as you want: the Dash will stay visible as long as you hover your mouse over it. The Dash/Overview will only close once you move your cursor away from the Dash area. This extension preserves the native Dash/Overview general aesthetics.
+
+## 🚀 Installation
+
+### Method 1: Via ZIP (Recommended)
+Go to the [Releases](https://github.com/cybeR-at/dash-hover-hold/releases) tab of this repository and download the latest `.zip` file. Open your terminal and install it using:
+
+```bash
+gnome-extensions install dash-hover-hold@cyber-at.github.com.shell-extension.zip
+```
+Log out and log back in to restart your session. Enable the "Dash Hover Hold" extension in the GNOME Extensions Manager.
+
+
+## Method 2: Manual Installation
+
+Run the following commands in your terminal:
+```Bash
+
+git clone https://github.com/cybeR-at/dash-hover-hold.git
+cd dash-hover-hold
+mkdir -p ~/.local/share/gnome-shell/extensions/dash-hover-hold@cyber-at.github.com
+cp extension.js metadata.json ~/.local/share/gnome-shell/extensions/dash-hover-hold@cyber-at.github.com/
+```
+After copying, log out of your session, log back in, and enable the extension.
+
+This extension was done with the help of Gemini Pro and I tested it thoroughly.
---
+++ extension.js
@@ -0,0 +1,148 @@
+import GLib from 'gi://GLib';
+import Clutter from 'gi://Clutter';
+import Shell from 'gi://Shell';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as AppDisplay from 'resource:///org/gnome/shell/ui/appDisplay.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+
+export default class DashHoverHoldExtension extends Extension {
+    enable() {
+        this._monitorId = null;
+        this._showingId = null;
+        this._hiddenId = null;
+        this._wasInDash = false; 
+        
+        const ext = this;
+
+        // 1. Intercept the app icon click
+        this._originalAppIconActivate = AppDisplay.AppIcon.prototype.activate;
+
+        AppDisplay.AppIcon.prototype.activate = function (button) {
+            let event = Clutter.get_current_event();
+            let modifiers = event ? event.get_state() : 0;
+            let isMiddleButton = button && button === Clutter.BUTTON_MIDDLE;
+            let isCtrlPressed = (modifiers & Clutter.ModifierType.CONTROL_MASK) !== 0;
+            
+            let openNewWindow = this.app.can_open_new_window() &&
+                                this.app.state === Shell.AppState.RUNNING &&
+                                (isCtrlPressed || isMiddleButton);
+
+            if (this.app.state === Shell.AppState.STOPPED || openNewWindow)
+                this.app.open_new_window(-1);
+            else
+                this.app.activate();
+
+            // If clicked on an app NOT in the Dash, close immediately.
+            if (!ext._isPointerInDashOrMenuOpen()) {
+                Main.overview.hide();
+            }
+        };
+
+        // 2. Start checking mouse position when Overview opens
+        this._showingId = Main.overview.connect('showing', () => {
+            this._wasInDash = false; 
+            
+            if (!this._monitorId) {
+                this._monitorId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 150, this._monitorLoop.bind(this));
+            }
+        });
+
+        // 3. Stop monitor when Overview closes
+        this._hiddenId = Main.overview.connect('hidden', () => {
+            this._cleanupMonitor();
+        });
+    }
+
+    _monitorLoop() {
+        if (!Main.overview.visible) {
+            this._monitorId = null;
+            return GLib.SOURCE_REMOVE;
+        }
+
+        // Evaluate whether the mouse is in the Dash OR if a context menu is open
+        let inDashOrMenu = this._isPointerInDashOrMenuOpen();
+
+        if (inDashOrMenu) {
+            this._wasInDash = true;
+        } else if (!inDashOrMenu && this._wasInDash) {
+            // The mouse left the Dash and there are no open menus. Close it!
+            Main.overview.hide();
+            this._monitorId = null;
+            return GLib.SOURCE_REMOVE;
+        }
+
+        return GLib.SOURCE_CONTINUE;
+    }
+
+    _isPointerInDashOrMenuOpen() {
+        // 1. Geometric Check (Dash Area)
+        let dash = Main.overview.dash;
+        if (dash) {
+            let [pointerX, pointerY] = global.get_pointer();
+            let [dashX, dashY] = dash.get_transformed_position();
+            let [dashWidth, dashHeight] = dash.get_transformed_size();
+            let margin = 10;
+            
+            if (pointerX >= dashX - margin && 
+                pointerX <= dashX + dashWidth + margin &&
+                pointerY >= dashY - margin && 
+                pointerY <= dashY + dashHeight + margin) {
+                return true;
+            }
+        }
+
+        // 2. GNOME Global Menu Manager Check
+        // If a popup menu (right-click) is active, keep the overview open
+        if (Main.popupMenuManager && Main.popupMenuManager.activeMenu) {
+            return true;
+        }
+
+        // 3. Deep check on Dash icons (Fallbacks for different GNOME versions)
+        if (dash && dash._box) {
+            let items = dash._box.get_children();
+            for (let item of items) {
+                // Attempt to get the icon, bypassing invisible container layers
+                let icon = item.child || (item.get_first_child ? item.get_first_child() : item);
+                if (icon) {
+                    let menu = icon.menu || icon._menu || icon.popupMenu;
+                    if (menu && menu.isOpen) return true;
+                }
+            }
+        }
+
+        // 4. Check the Show Apps button
+        if (dash && dash._showAppsIcon) {
+            let menu = dash._showAppsIcon.menu || dash._showAppsIcon._menu || dash._showAppsIcon.popupMenu;
+            if (menu && menu.isOpen) return true;
+        }
+
+        return false;
+    }
+
+    _cleanupMonitor() {
+        if (this._monitorId) {
+            GLib.Source.remove(this._monitorId);
+            this._monitorId = null;
+        }
+        this._wasInDash = false;
+    }
+
+    disable() {
+        if (this._originalAppIconActivate) {
+            AppDisplay.AppIcon.prototype.activate = this._originalAppIconActivate;
+            this._originalAppIconActivate = null;
+        }
+
+        if (this._showingId) {
+            Main.overview.disconnect(this._showingId);
+            this._showingId = null;
+        }
+
+        if (this._hiddenId) {
+            Main.overview.disconnect(this._hiddenId);
+            this._hiddenId = null;
+        }
+
+        this._cleanupMonitor();
+    }
+}
